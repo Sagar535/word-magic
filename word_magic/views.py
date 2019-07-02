f34908762cb9caae238b474dc231007b0a3c1692
Sagar <070bct535@ioe.edu.np>
2019-07-02 21:03:20 +0545

    minor fixes and word count done

--- word_magic/views.py
+++ word_magic/views.py
@@ -2,5 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-	return render(request, '../templates/home.html.plim')
+	return render(request, 'home.html', {'count': 0, 'word_dict': {}})
 
+def count(request):
+	print("Inside the count")
+	words = request.GET["full_text"]
+	count = len(words)
+	word_dict = {}
+	for c in words:
+		if c in word_dict:
+			word_dict[c] += 1
+		else:
+			word_dict[c] = 1
+	return render(request, 'home.html', {
+		'count': count, 
+		'words': words, 
+		"word_dict": word_dict
+		})
+ 
