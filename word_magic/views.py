f3386bdea23eb6638c1f0a7eb797fa2c6a758441
Sagar <070bct535@ioe.edu.np>
2019-07-03 21:48:08 +0545

    cleans w-spaces, and makes all lower case

--- word_magic/views.py
+++ word_magic/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import re
 
 def index(request):
 	return render(request, 'home.html', {'count': 0, 'word_dict': {}})
 
 def count(request):
-	print("Inside the count")
-	words = request.GET["full_text"]
+	original_text = request.GET["full_text"]
+	words = clean_words(original_text)
 	count = len(words)
 	word_dict = {}
 	for c in words:
@@ -16,7 +17,11 @@ def count(request):
 			word_dict[c] = 1
 	return render(request, 'home.html', {
 		'count': count, 
-		'words': words, 
+		'words': original_text, 
 		"word_dict": word_dict
 		})
  
+def clean_words(word_to_be_cleaned):
+	pattern = re.compile(r'\s+')
+	ws_removed =  re.sub(pattern, '', word_to_be_cleaned)
+	return ws_removed.lower()
